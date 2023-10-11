65e22afcb11e153563b1372f2b0c64a19e008ae9
one-tenth <11056010@ntub.edu.tw>
2023-10-11 16:18:04 +0800

    123546

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from mysite.models import Post
+from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    now=datetime.now()
+    return render(request,'index.html',locals())
+def showpost(request,slug):
+    post=Post.objects.get(slug=slug)#selet*from post where slug=%slug
+    return render(request,'post.html',locals())
+'''
+def homepage(request):
+    posts = Post.objects.all()#select*from post
+    post_lists=list()
+    for counter,post in enumerate(posts):#enumerate 
+        post_lists.append(f'No.{counter} {post} <br>')#br換行
+    return HttpResponse(post_lists)
+    '''
