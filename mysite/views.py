f6acd8ea9771ac6ef8213741b0fa87e90ad0b622
one-tenth <11056010@ntub.edu.tw>
2023-10-18 16:29:42 +0800

    1234556

--- mysite/views.py
+++ mysite/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from mysite.models import Post
 from django.http import HttpResponse
 from datetime import datetime
+from django.shortcuts import redirect
 # Create your views here.
 def homepage(request):
     posts = Post.objects.all()
     now=datetime.now()
     return render(request,'index.html',locals())
 def showpost(request,slug):
-    post=Post.objects.get(slug=slug)#selet*from post where slug=%slug
-    return render(request,'post.html',locals())
+    try:
+        post=Post.objects.get(slug=slug)#selet*from post where slug=%slug
+        if post != None:
+            return render(request,'post.html',locals())
+        else:
+            return redirect("/")
+    except:
+            return redirect("/")
+    #select * from post where slug=%slug
 '''
 def homepage(request):
     posts = Post.objects.all()#select*from post
@@ -17,4 +25,4 @@ def homepage(request):
     for counter,post in enumerate(posts):#enumerate 
         post_lists.append(f'No.{counter} {post} <br>')#br換行
     return HttpResponse(post_lists)
-    '''
+'''
