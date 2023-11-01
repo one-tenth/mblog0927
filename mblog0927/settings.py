9b825307c09f420c707eca8da45b219885011fc0
one-tenth <11056010@ntub.edu.tw>
2023-11-01 16:16:57 +0800

    123

--- mblog0927/settings.py
+++ mblog0927/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'mysite'
+    'mysite',#每次加一個app都要加
 ]
 
 MIDDLEWARE = [
